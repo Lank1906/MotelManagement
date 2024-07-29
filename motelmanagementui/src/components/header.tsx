11d549403bdbcb9451f7d8ed0d7107daad0a9e53
Lank1906 <buuixuanhoangc@gmail.com>
2024-07-29 20:49:10 +0700

    update header

--- motelmanagementui/src/components/header.tsx
+++ motelmanagementui/src/components/header.tsx
@@ -1,10 +1,7 @@
-import Menu from "./menu";
-
 export default function Header() {
   return (
     <header>
       <div className="logo">OMS.</div>
-      <Menu />
       <ul className="horizontal">
         <li className="btn">Đăng Ký</li>
         <li className="btn">Đăng Nhập</li>
