19147971ee31b8f68d46353d70def1597c523de4
Lank1906 <buuixuanhoangc@gmail.com>
2024-07-31 21:50:19 +0700

    add action for arrow

--- motelmanagementui/src/App.tsx
+++ motelmanagementui/src/App.tsx
@@ -3,21 +3,38 @@ import Menu from "./components/menu";
 import Info from "./components/info";
 import Content from "./components/content";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [leftStatus,setLeftStatus]=useState('');
+  const [rightStatus,setRightStatus]=useState('');
   return (
     <>
       <Header />
       <div id="body">
-        <section id="menu-left">
+        <section id="menu-left" style={{width:leftStatus}}>
           <Menu />
         </section>
-        <div className="arrow">
-          <i className="fa-solid fa-angles-right"></i>
+        <div className="arrow" onClick={()=>{
+          if(window.innerWidth<860 && leftStatus=='350px'){
+            setLeftStatus('')
+          }
+          else{
+            setLeftStatus('350px')
+          }
+        }}>
+          <i className="fa-solid fa-angles-right" ></i>
         </div>
         <Content />
-        <Info />
-        <div className="arrow2">
+        <Info widthE={rightStatus}/>
+        <div className="arrow2" onClick={()=>{
+          if(window.innerWidth<860 && rightStatus=='350px'){
+            setRightStatus('')
+          }
+          else{
+            setRightStatus('350px')
+          }
+        }}>
           <i className="fa-solid fa-angles-left"></i>
         </div>
       </div>
--- motelmanagementui/src/components/info.tsx
+++ motelmanagementui/src/components/info.tsx
@@ -1,4 +1,8 @@
-export default function Info() {
-    return <div className="info">Info</div>;
+interface infoProps {
+  widthE:string;
+}
+
+export default function Info(props:infoProps) {
+    return <div className="info" style={{width:props.widthE,padding:props.widthE=='350px'?"0px 20px":0}}>Info</div>;
   }
   
