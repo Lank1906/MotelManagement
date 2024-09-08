f3e10543e13ac9d864ae486278302ce961158fa0
Lank1906 <buuixuanhoangc@gmail.com>
2024-09-08 09:16:05 +0700

    add some note

--- motelmanagementui/src/interface/room_context_type.ts
+++ motelmanagementui/src/interface/room_context_type.ts
@@ -1,5 +1,5 @@
 import { RoomType } from "./room_type";
-
+// dont use
 export interface RoomContextType{
     list:RoomType[],
     setList:React.Dispatch<React.SetStateAction<RoomType[]>>,
--- motelmanagementui/src/libs/room_context.tsx
+++ motelmanagementui/src/libs/room_context.tsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 import { RoomType } from "../interface/room_type";
 import { ContextProviderProps } from "../interface/context_provider_props";
 import { RoomContextType } from "../interface/room_context_type";
-
+// dont use
 const RoomContext=createContext<RoomContextType|undefined>(undefined)
 
 function RoomProvider({children}:ContextProviderProps){
