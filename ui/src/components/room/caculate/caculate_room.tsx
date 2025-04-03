d4aa552159d3c3fa9105d3f7f0160ae01e9d8061
Lank1906 <buuixuanhoangc@gmail.com>
2025-04-03 17:18:03 +0700

    add: report

--- ui/src/components/room/caculate/caculate_room.tsx
+++ ui/src/components/room/caculate/caculate_room.tsx
@@ -12,18 +12,18 @@ export default function RoomCaculate() {
     const [object, setObject] = useState<BillType>()
     const [electric, setEletric] = useState<number>(0)
     const [water, setWater] = useState<number>(0)
-    const [type,setType]=useState<number>(0)
+    const [type, setType] = useState<number>(0)
     const [filter, setFilter] = useState<Date | undefined>(undefined)
 
     const context = useContext(MyContext)
     const dataContext = useContext(DataContext)
     const announceContext = useContext(AnnounceContext)
-    const loadingContext=useContext(LoadingContext)
+    const loadingContext = useContext(LoadingContext)
 
     let timeout: ReturnType<typeof setTimeout> | null = null;
 
     useEffect(() => {
-        if(dataContext?.id==-1)
+        if (dataContext?.id == -1)
             return
         loadingContext?.setStatus(false)
         GetFetch('calculate/' + dataContext?.id,
@@ -37,10 +37,10 @@ export default function RoomCaculate() {
                 announceContext?.setClose(true)
                 loadingContext?.setStatus(false)
             })
-    },[dataContext?.id])
+    }, [dataContext?.id])
 
-    function calculate(typez:number) {
-        if(water<0||electric<=0||typez <0||typez>2||dataContext?.id==-1)
+    function calculate(typez: number) {
+        if (water < 0 || electric <= 0 || typez < 0 || typez > 2 || dataContext?.id == -1)
             return
         loadingContext?.setStatus(true)
         setType(typez)
@@ -60,8 +60,8 @@ export default function RoomCaculate() {
         )
     }
 
-    function payed(){
-        if(water<0||electric<=0||type <0||type>2||dataContext?.id==-1)
+    function payed() {
+        if (water < 0 || electric <= 0 || type < 0 || type > 2 || dataContext?.id == -1)
             return
         loadingContext?.setStatus(true)
         PutFetch("calculate/" + dataContext?.id,
@@ -92,21 +92,49 @@ export default function RoomCaculate() {
             return;
         }
 
-        if(timeout){
+        if (timeout) {
             clearTimeout(timeout)
         }
+
+        timeout = setTimeout(() => {
+            if (dataContext?.id === -1 || !filter)
+                return
+            loadingContext?.setStatus(true)
+            GetFetch('calculate/' +`${filter.getFullYear()}-${filter.getMonth() + 1}-${filter.getDate()}`+'/' + dataContext?.id,
+                (data: BillType) => {
+                    setObject(data)
+                },
+                context?.data,
+                (data: any) => {
+                    announceContext?.setMessage(data.message)
+                    announceContext?.setType("danger")
+                    announceContext?.setClose(true)
+                    loadingContext?.setStatus(false)
+                })
+        }, 3000)
     }
 
-    async function filterRefresh(){
+    async function filterRefresh() {
         setFilter(undefined)
-        //loadingContext?.setStatus(true)
+        loadingContext?.setStatus(true)
+        GetFetch('calculate/' + dataContext?.id,
+            (data: BillType) => {
+                setObject(data)
+            },
+            context?.data,
+            (data: any) => {
+                announceContext?.setMessage(data.message)
+                announceContext?.setType("danger")
+                announceContext?.setClose(true)
+                loadingContext?.setStatus(false)
+            })
     }
 
     return (
         <>
             <div className="component">
                 <label htmlFor="filter">Tra hóa đơn cũ:</label><br />
-                <input type="date" className="input" name="filter" value={filter?filter.toISOString().split('T')[0]:''} onChange={(e)=>filterChange(e)} />
+                <input type="date" className="input" name="filter" value={filter ? filter.toISOString().split('T')[0] : ''} onChange={(e) => filterChange(e)} />
                 <div className="btn" onClick={filterRefresh}><i className="fa-solid fa-arrows-rotate"></i></div>
             </div>
             <table id="list-service">
@@ -121,7 +149,7 @@ export default function RoomCaculate() {
                     {object?.data ? object.data.map((item: BillDetailType) => <tr key={item.category}><td>{item.category}</td><td>{item.price}x{item.times}</td><td>{item.sum}</td></tr>) : "Loading"}
                 </tbody>
             </table>
-            <div className="service-action" style={{display:filter?'none':'block'}}>
+            <div className="service-action" style={{ display: filter ? 'none' : 'block' }}>
                 <div className="block">
                     <label htmlFor="name">Số điện: </label><br />
                     <input type="number" className="input bg-white" name="name" value={electric} onChange={(e) => setEletric(parseInt(e.target.value))} />
@@ -131,15 +159,15 @@ export default function RoomCaculate() {
                     <input type="number" className="input bg-white" name="name" value={water} onChange={(e) => setWater(parseInt(e.target.value))} readOnly={object?.water_follow ? true : false} />
                 </div>
             </div>
-            <div className="service-action" style={{display:filter?'none':'block'}}>
-                <div className="btn delete" onClick={()=>calculate(2)}><i className="fa-solid fa-arrow-right-from-bracket"></i> Chuyển đi</div>
-                <div className="btn add" onClick={()=>calculate(1)}><i className="fa-solid fa-rotate"></i> Thanh toán</div>
-                <div className="btn update" onClick={()=>calculate(0)}><i className="fa-solid fa-arrow-left"></i> Chuyển đến</div>
+            <div className="service-action" style={{ display: filter ? 'none' : 'block' }}>
+                <div className="btn delete" onClick={() => calculate(2)}><i className="fa-solid fa-arrow-right-from-bracket"></i> Chuyển đi</div>
+                <div className="btn add" onClick={() => calculate(1)}><i className="fa-solid fa-rotate"></i> Thanh toán</div>
+                <div className="btn update" onClick={() => calculate(0)}><i className="fa-solid fa-arrow-left"></i> Chuyển đến</div>
             </div>
-            <div className="service-action" style={{display:filter?'none':'block'}}>
+            <div className="service-action" style={{ display: filter ? 'none' : 'block' }}>
                 <div className="btn add" onClick={payed}><i className="fa-solid fa-money-bill"></i> Xác nhận thanh toán</div>
             </div>
-            {loadingContext?.status?<Loader/>:''}
+            {loadingContext?.status ? <Loader /> : ''}
         </>
     )
 }
